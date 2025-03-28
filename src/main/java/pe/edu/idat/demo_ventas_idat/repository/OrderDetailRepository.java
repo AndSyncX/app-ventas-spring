7242c9fc884398132b9fc330d0cdc267b1a03d29
AndSyncX <fernandezp.joe@gmail.com>
2025-03-27 22:32:41 -0500

    Service

--- src/main/java/pe/edu/idat/demo_ventas_idat/repository/OrderDetailRepository.java
+++ src/main/java/pe/edu/idat/demo_ventas_idat/repository/OrderDetailRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pe.edu.idat.demo_ventas_idat.model.OrderDetailModel;
 import pe.edu.idat.demo_ventas_idat.model.ProductOrderId;
 
+// Son componentes -> Facilita que los frameworks se puedan comunicar de manera mas facil y eficiente
 public interface OrderDetailRepository extends JpaRepository<OrderDetailModel, ProductOrderId> {
 
 }
--- src/main/java/pe/edu/idat/demo_ventas_idat/service/CategoryService.java
+++ src/main/java/pe/edu/idat/demo_ventas_idat/service/CategoryService.java
@@ -1,5 +1,23 @@
 package pe.edu.idat.demo_ventas_idat.service;
+import pe.edu.idat.demo_ventas_idat.model.CategoryModel;
+import pe.edu.idat.demo_ventas_idat.repository.CategoryRepository;
+
+import java.util.List;
 
 public class CategoryService {
 
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<CategoryModel> obtenerCategorias(){
+        return categoryRepository.findAll();
+    }
+
+    public void guardarCategoria(CategoryModel category){
+        // Insert Into
+        categoryRepository.save(category);
+    }
 }
