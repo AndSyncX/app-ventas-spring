cd070ec8b086365de6a8c9e65d9adfd0abee2d7d
AndSyncX <fernandezp.joe@gmail.com>
2025-04-03 22:33:34 -0500

    Advanced

--- src/main/java/pe/edu/idat/demo_ventas_idat/repository/ProductRepository.java
+++ src/main/java/pe/edu/idat/demo_ventas_idat/repository/ProductRepository.java
@@ -1,7 +1,23 @@
 package pe.edu.idat.demo_ventas_idat.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pe.edu.idat.demo_ventas_idat.model.ProductModel;
 
 public interface ProductRepository extends JpaRepository<ProductModel, Integer> {
-
+    @Query(value = """
+            UPDATE products SET
+            productname = :productname,
+            suppleirid = :suppleirid:
+            categoryid = :categoryid:
+            discontinued = :discontinued
+            WHERE productid = :productid
+            """, nativeQuery = true)
+    void actualizarProductoParcial(
+            @Param("productname") String productname,
+            @Param("suppleirid") Integer suppleirid,
+            @Param("categoryid") Integer categoryid,
+            @Param("discontinued") boolean discontinued,
+            @Param("productid") Integer productid
+    );
 }
